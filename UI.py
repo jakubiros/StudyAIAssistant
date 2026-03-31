a04150811c9280bf024c918047bf8ff566ac0ad7
jakubbiros <jakub.biros@microsoft.wsei.edu.pl>
2026-03-31 20:25:30 +0200

    Added option for saving text nad pdfs

--- UI.py
+++ UI.py
@@ -26,6 +26,25 @@ def main():
     with st.expander('Manage education materials (Click, to expand)', expanded=not bool(st.session_state.context_text)):
         tab_text,tab_pdf=st.tabs(['Paste text', 'Upload PDF'])
 
+        with tab_text:
+            user_text=st.text_area('Paste notes:', height=350, placeholder='Copy and paste your text here...')
+            if st.button('Save text'):
+                if user_text.strip():
+                    st.session_state.context_text = user_text
+                    st.session_state.messages = []
+                    st.success('Notes saved! You can use chat. ')
+                else:
+                    st.warning('Area is empty.')
+
+        with tab_pdf:
+            uploaded_file=st.file_uploader('Drag and drop PDF file', type='pdf')
+            if uploaded_file:
+                if st.button('Load PDF'):
+                    with st.spinner('PDF Processing...'):
+                        extracted_text=DocumentReader.extract_text_pdf(uploaded_file.read())
+                        st.session_state.context_text = extracted_text
+                        st.session_state.messages = []
+                    st.success(f'Loaded {uploaded_file.name}')
 
 if __name__ == "__main__":
     main()
