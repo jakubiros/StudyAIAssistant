52376ea23487b249e6e2ef2a3612f94d436d9d57
jakubbiros <jakub.biros@microsoft.wsei.edu.pl>
2026-03-31 19:58:44 +0200

    Added columns in UI

--- UI.py
+++ UI.py
@@ -15,6 +15,17 @@ def main():
     if "context_text" not in st.session_state:
         st.session_state.context_text = ""
 
+    col1,col2=st.columns([4,1])
+    with col1:
+        st.title('Your AI Assistant')
+    with col2:
+        if st.button('Clear Chat', use_container_width=True):
+            st.session_state.messages = []
+            st.rerun()
+
+    with st.expander('Manage education materials (Click, to expand)', expanded=not bool(st.session_state.context_text)):
+        tab_text,tab_pdf=st.tabs(['Paste text', 'Upload PDF'])
+
 
 if __name__ == "__main__":
     main()
